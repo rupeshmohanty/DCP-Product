aec5fcade3934518106acbe5e1fc36d8e201daf8
Rupesh Mohanty <rupeshmohanty67@gmail.com>
2023-09-08 16:51:19 +0530

    anchor replaced with Link

--- src/Components/Admin/Product/sub-components/Navbar.jsx
+++ src/Components/Admin/Product/sub-components/Navbar.jsx
@@ -4,27 +4,27 @@ const Navbar = () => {
     return(
         <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <Link class="navbar-brand">Navbar</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <Link class="nav-link active" aria-current="page">Home</Link>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <Link class="nav-link">Link</Link>
                     </li>
                     <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <Link class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Dropdown
-                    </a>
+                    </Link>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><Link class="dropdown-item">Action</Link></li>
+                        <li><Link class="dropdown-item">Another action</Link></li>
                         <li><hr class="dropdown-divider"/></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><Link class="dropdown-item">Something else here</Link></li>
                     </ul>
                     </li>
                 </ul>
