46cb6a08db56fd824d22381426984295b7b1542b
Rupesh Mohanty <rupeshmohanty67@gmail.com>
2023-09-16 01:50:07 +0530

    Product list view component api integrated

--- src/Components/Admin/Product/sub-components/ProductViewComponent.jsx
+++ src/Components/Admin/Product/sub-components/ProductViewComponent.jsx
@@ -1,97 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const ProductViewComponent = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://18.132.190.180:8081/products')
+        .then((res) => {
+            setProducts(res.data)
+        })
+    }, [])
+
+    console.log(products);
+
     return(
         <>
-            {/* Row 1 */}
             <div className="row">
-                <div className="col-md-4">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                { products.map((product) => {
+                    return(
+                        <div className="col-md-4" id = { product.id } style={{ marginTop: '10px' }}>
+                            <div className="card" style={{ width: '18rem' }}>
+                                <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{ product.productCode }</h5>
+                                    <p className="card-text">{ product.productDescription }</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            {/* Row 2 */}
-            <div className="row" style={{ paddingTop: '10px' }}>
-                <div className="col-md-4">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            {/* Row 3 */}
-            <div className="row" style={{ paddingTop: '10px' }}>
-                <div className="col-md-4">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://thumbs.dreamstime.com/b/loading-products-line-icon-vector-illustration-loading-products-linear-concept-sign-loading-products-linear-icon-concept-117095027.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
+                    )
+                }) }
             </div>
         </>
     )
